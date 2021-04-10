e5af979fd652e9900b65f40d3ca637131d536a57
luisangeluis <luisangeluis@gmail.com>
2021-04-10 17:32:52 -0500

    Se revisa video 6 components son como metodos solo que ya despues de pasar los argumentos ya no se pueden modificar son inmutables (De solo lectura)

---
+++ components/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+//components
+import Person from './components/Person';
+
+function App() {
+  return (
+    <Person a="luis" edad={10}></Person>
+  );
+}
+
+export default App;
---
+++ components/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Person = ({nombre,edad}) =>{
+
+
+    return(
+        <p>Soy {nombre} y tengo {edad} años</p>
+    )
+}
+
+export default Person;
