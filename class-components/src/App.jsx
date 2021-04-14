c9e6a49d7c219388af6cebb2059a15d788d926ae
luisangeluis <luisangeluis@gmail.com>
2021-04-13 21:43:16 -0500

    video 7 class components terminado

--- class-components/src/App.jsx
+++ class-components/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+/*
 class App extends Component {
   constructor(props) {
     console.dir(props);
@@ -16,5 +16,29 @@ class App extends Component {
     );
   }
 }
+*/
+class App extends Component {
+  //Inicializar el state con constructor
+  /*
+  constructor(props) {
+    super(props);
+    this.state = { number: 5 };
+    console.log(this.state);
+  }
+  */
+  //iniciar state fuera de los metodos
+  state={
+    number: 5
+  }
+  render() {
+    console.log(this.state);
+
+    const {number, name} =this.props;
+    return (
+      <h1>Class component {number} {name}</h1>
+    );
+  }
+}
 
 export default App;
+
