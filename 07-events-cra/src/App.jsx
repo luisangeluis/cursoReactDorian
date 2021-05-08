3e8b5e59ba279f6ed851eae9caccbf47e0a8d448
luisangeluis <luisangeluis@gmail.com>
2021-05-07 22:50:20 -0500

    Se terminó el video 9 eventos en react terminado

--- 07-events-cra/src/App.jsx
+++ 07-events-cra/src/App.jsx
@@ -5,28 +5,34 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+    //Para aceptar la funciones cuando no puedes usar funcion de flecha
+    this.handleClick = this.handleClick.bind(this);
   }
+
   state={
     number:0
   }
+
   handleClick=()=>{
     
     this.setState({
       number:this.state.number+1
     })
-    // console.log(this);
+    console.log(this);
     console.log(this.state);
   }
+
   render() {
 
     return (
       <>
-      <h1>Events</h1>
+      <h1>Event</h1>
       <button onClick={this.handleClick}>click me</button>
       <p>{this.state.number}</p>
       </>
     );
   }
+
 }
 
 export default App;
